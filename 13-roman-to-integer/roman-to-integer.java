952797fda45710f4e36551c0c31fa71454c445d0
kamalan2k4 <117959444+kamalan2k4@users.noreply.github.com>
2024-08-19 19:10:08 +0530

    Time: 2 ms (100.00%) | Memory: 44.9 MB (15.83%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,25 +1,49 @@
 class Solution {
     public int romanToInt(String s) {
-         int answer = 0, number = 0, prev = 0;
-
-    for (int j = s.length() - 1; j >= 0; j--) {
-        switch (s.charAt(j)) {
-            case 'M' -> number = 1000;
-            case 'D' -> number = 500;
-            case 'C' -> number = 100;
-            case 'L' -> number = 50;
-            case 'X' -> number = 10;
-            case 'V' -> number = 5;
-            case 'I' -> number = 1;
+        int number=0;
+        int ans=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)=='I')
+            {
+            number=1;
+            // break;
+            }
+           else if(s.charAt(i)=='V')
+            {
+            number=5;
+            // break;
+            }
+            else if(s.charAt(i)=='X')
+            {
+            number=10;
+            // break;
+            }
+            else if(s.charAt(i)=='L')
+            {
+            number=50;
+            // break;
+            }
+            else if(s.charAt(i)=='C')
+            {
+            number=100;
+            // break;
+            }
+            else if(s.charAt(i)=='D')
+            {
+            number=500;
+            // break;
+            }
+            else if(s.charAt(i)=='M')
+            {
+            number=1000;
+            // break;
+            }
+            if(4*number<ans)
+            ans-=number;
+            else
+            ans+=number;
         }
-        if (number < prev) {
-            answer -= number;
-        }
-        else {
-            answer += number;
-        }
-        prev = number;
-    }
-    return answer;
+        return ans;
     }
 }
