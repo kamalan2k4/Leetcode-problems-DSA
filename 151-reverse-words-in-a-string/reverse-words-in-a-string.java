3626f97f441e25ffc09552f0a009c3e4a2b15359
kamalan2k4 <117959444+kamalan2k4@users.noreply.github.com>
2024-08-21 19:12:42 +0530

    Time: 12 ms (27.59%) | Memory: 45.2 MB (15.47%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,47 +1,69 @@
 class Solution {
     public String reverseWords(String s) {
-        int l=0,r=s.length()-1;
-        String temp="";
-        String ans="";
-        while(s.charAt(r)==' ')r--;
-        while(l<=r)
+        
+        // int l=0,r=s.length()-1;
+        // String temp="";
+        // String ans="";
+        // // while(s.charAt(r)==' ')r--;
+        
+        // while(l<=r)
+        // {
+        //     char c=s.charAt(l);
+        //     if(c!=' ')
+        //     {
+        //        temp+=c;
+        //     }
+        //     else if(c==' ')
+        //     {
+        //         if(!ans.equals(""))
+        //         {
+        //             if(ans.charAt(0)==' ')
+        //             ans= temp + ans;
+        //             else
+        //             ans= temp + " " + ans;
+        //         }
+        //         else
+        //         {
+        //             ans=temp;
+        //         }
+        //     temp="";
+        //     }
+        //     l++;
+        // }
+        // if(!temp.equals(""))
+        // {
+        //     if(!ans.equals(""))
+        //     {
+        //         if(ans.charAt(0)==' ')
+        //         ans= temp + ans;
+        //         else
+        //         ans= temp + " " + ans;
+        //     }
+        //     else
+        //     {
+        //         ans = temp;
+        //     }
+        // }
+        // return ans;
+        String ans=s.trim();
+        String str[]=ans.split("\s+");
+        int l=0;int h=str.length-1;
+        while(l<h)
         {
-            char c=s.charAt(l);
-            if(c!=' ')
-            {
-               temp+=c;
-            }
-            else if(c==' ')
-            {
-                if(!ans.equals(""))
-                {
-                    if(ans.charAt(0)==' ')
-                    ans= temp + ans;
-                    else
-                    ans= temp + " " + ans;
-                }
-                else
-                {
-                    ans=temp;
-                }
-            temp="";
-            }
+            String temp=str[l];
+            str[l]=str[h];
+            str[h]=temp;
             l++;
+            h--;
         }
-        if(!temp.equals(""))
-        {
-            if(!ans.equals(""))
-            {
-                if(ans.charAt(0)==' ')
-                ans= temp + ans;
-                else
-                ans= temp + " " + ans;
-            }
-            else
-            {
-                ans = temp;
-            }
-        }
-        return ans;
+       String answer="";
+       for(int i=0;i<str.length;i++)
+       {
+        if(i==str.length-1)
+        answer=answer+str[i];
+        else
+        answer=answer+str[i]+" ";
+       }
+       return answer;
     }
 }
