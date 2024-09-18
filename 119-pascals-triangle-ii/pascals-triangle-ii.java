5cbbe044acbce428ea20cffdb26d1af89a0ff1cf
kamal <117959444+kamalan2k4@users.noreply.github.com>
2024-09-18 18:49:55 +0530

    Time: 1 ms (67.14%) | Memory: 41.3 MB (12.68%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+         List<Integer> result = new ArrayList<>(Collections.nCopies(rowIndex + 1, 0));
+        result.set(0, 1);
+
+        for (int row = 1; row <= rowIndex; ++row) {
+            result.set(row, 1);
+
+            for (int index = row - 1; index > 0; --index) {
+                result.set(index, result.get(index) + result.get(index - 1));
+            }
+        }
+
+        return result;
+    }
+}
