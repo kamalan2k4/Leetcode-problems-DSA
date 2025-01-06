cc14f67c408a84824f18f87f41e0dae9fcd40ac5
kamal <117959444+kamalan2k4@users.noreply.github.com>
2025-01-06 23:04:20 +0530

    Update maximum-width-ramp.java

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         Stack<Integer> stack = new Stack<>();
         
-        // Step 1: Build a decreasing stack of indices
+        // Step 1: Build a decreasing stack of indices 
         for (int i = 0; i < n; ++i) {
             if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
                 stack.push(i);
