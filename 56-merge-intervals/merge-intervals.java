ece2385f9f5eaac452a92ad425dd4e2b80d4c832
kamal <117959444+kamalan2k4@users.noreply.github.com>
2025-05-02 11:12:23 +0530

    Time: 8 ms (86.61%) | Memory: 46.6 MB (54.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,16 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-          if (arr.length == 0) {
-            return new int[0][];
+    public int[][] merge(int[][] a) {
+        Arrays.sort(a,(x,y)-> x[0]-y[0]);
+        
+        List<int[]> ans=new ArrayList<>();
+        ans.add(a[0]);
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i][0] > ans.get(ans.size()-1)[1])
+            ans.add(a[i]);
+            else
+            ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],a[i][1]);
         }
-
-        
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-
-        
-        List<int[]> res = new ArrayList<>(); 
-        res.add(arr[0]);
-
-        
-        for (int i = 1; i < arr.length; i++) {
-    
-            int[] prev = res.get(res.size() - 1);
-            int[] current = arr[i];
-
-            // Check if the current interval overlaps with the previous one
-            if (current[0] <= prev[1]) { // Overlapping intervals, merge them
-                prev[1] = Math.max(prev[1], current[1]);
-            } else { // Non-overlapping interval, add to the list
-                res.add(current);
-            }
-        }
-
-        // Convert the list of intervals to a 2D array
-        return res.toArray(new int[res.size()][]);
-        
-
+        return ans.toArray(new int[ans.size()][]);
     }
 }
